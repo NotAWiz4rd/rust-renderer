27ab4ec685960f08bee455462ef2003b55ef421e
NotAWiz4rd <xamasa98@gmail.com>
2022-07-25 23:14:38 +0200

    added writing projectile path to ppm-file

--- src/projectile.rs
+++ src/projectile.rs
@@ -1,15 +1,25 @@
+use crate::canvas::canvas;
+use crate::colour::colour;
 use crate::tuple::{point_i, Tuple, vector, vector_i};
+use crate::util::write_file;
 
 pub fn run_projectile_simulation() {
-    let mut proj = Projectile { position: point_i(0, 2, 0), velocity: vector_i(1, 1, 0).normalize() };
+    let mut proj = Projectile { position: point_i(0, 2, 0), velocity: vector(1.0, 1.8, 0.0).normalize() * 11.25 };
     let environ = Environment { gravity: vector(0.0, -0.1, 0.0), wind: vector(-0.01, 0.0, 0.0) };
+    let mut canvas = canvas(900, 550);
 
     let mut tick_counter = 0;
     while proj.position.y > 0.0 {
         tick_counter += 1;
         tick(&environ, &mut proj);
+        let x = proj.position.x.round() as u32;
+        let y = 550 - proj.position.y.round() as u32;
+        if x < 900 && y < 550 {
+            canvas.write_pixel(proj.position.x.round() as u32, 550 - proj.position.y.round() as u32, colour(1.0, 0.0, 0.0));
+        }
         println!("{:?}", proj)
     }
+    write_file("projectile-simulation.ppm", &canvas.to_ppm()).expect("TODO: panic message");
     println!("Projectile flew for {} ticks.", tick_counter)
 }
 
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use std::io::Write;
+
 pub fn a_equal_b(a: f64, b: f64) -> bool {
     a.abs() - b.abs() < 1e-10
+}
+
+pub fn write_file(filepath: &str, content: &String) -> std::io::Result<()> {
+    let mut file = File::create(filepath)?;
+    file.write_all(content.as_ref())?;
+    Ok(())
 }
